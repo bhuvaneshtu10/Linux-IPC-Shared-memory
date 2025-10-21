0c4ad93cd693768cdccff38e8c71923f334602e5
BTU <btu100407@localhost.localdomain>
2025-10-21 08:20:47 +0200

    success

--- sem.c
+++ sem.c
@@ -1,28 +1,3 @@
-# Linux-IPC-Shared-memory
-Ex06-Linux IPC-Shared-memory
-
-# AIM:
-To Write a C program that illustrates two processes communicating using shared memory.
-
-# DESIGN STEPS:
-
-### Step 1:
-
-Navigate to any Linux environment installed on the system or installed inside a virtual environment like virtual box/vmware or online linux JSLinux (https://bellard.org/jslinux/vm.html?url=alpine-x86.cfg&mem=192) or docker.
-
-### Step 2:
-
-Write the C Program using Linux Process API - Shared Memory
-
-### Step 3:
-
-Execute the C Program for the desired output. 
-
-# PROGRAM:
-
-## Write a C program that illustrates two processes communicating using shared memory.
-
-```
 //sem.c
 #include <stdio.h>
 #include <stdlib.h>
@@ -131,16 +106,3 @@ int main() {
     }
 }
 
-```
-
-
-
-## OUTPUT
-
-
-![Alt text](os_exp6_1.png)
-![Alt text](os_exp6_2.png)
-
-
-# RESULT:
-The program is executed successfully.
